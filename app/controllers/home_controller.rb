997c41448c042d7018d31ac22a25aa4e489175e7
Matt Lummus <matthew.w.lummus@gmail.com>
2014-06-24 15:56:06 -0500

    Removed Adjucnts data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
     @hops = $brewery_db.hops.all
     @fermentables = $brewery_db.fermentables.all
     @yeasts = $brewery_db.yeasts.all
-    @adjuncts = $brewery_db.adjuncts.all
+    #@adjuncts = $brewery_db.adjuncts.all
   end
 end
