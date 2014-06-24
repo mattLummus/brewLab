9a095877af84265a6a7f3a96c74af4b38d17f7f0
Matt Lummus <matthew.w.lummus@gmail.com>
2014-06-24 13:36:33 -0500

    Added other data types to index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
     @styles = $brewery_db.styles.all
     @hops = $brewery_db.hops.all
     @fermentables = $brewery_db.fermentables.all
+    @yeasts = $brewery_db.yeasts.all
+    @adjuncts = $brewery_db.adjuncts.all
   end
 end
