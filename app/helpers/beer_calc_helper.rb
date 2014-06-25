e2979088dcdd39a6c93afc80b517c3968d2fe654
Matt Lummus <matthew.w.lummus@gmail.com>
2014-06-25 11:35:27 -0500

    Added Beer Style Controller

---
+++ app/helpers/beer_calc_helper.rb
@@ -0,0 +1,23 @@
+#example data
+module BeerCalcHelper
+
+  #alpha acid units
+  def aau
+
+  end
+
+  #(malt) points per gallon
+  def ppg
+
+  end
+
+  def utilization
+
+  end
+
+  #internationl bitterness units
+  def ibu
+
+  end
+
+end
