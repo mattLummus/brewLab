87f966b8abbcec72d92c5e903f0c87be5f47a2cc
Matt Lummus <matthew.w.lummus@gmail.com>
2014-06-25 12:44:40 -0500

    Removed Commented Code and nil beer style fields

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
     @styles = $brewery_db.styles.all
-    #@hops = $brewery_db.hops.all
-    #@fermentables = $brewery_db.fermentables.all
-    #@yeasts = $brewery_db.yeasts.all
-    #@adjuncts = $brewery_db.adjuncts.all
   end
 end
--- app/controllers/style_controller.rb
+++ app/controllers/style_controller.rb
@@ -3,6 +3,9 @@ class StyleController < ApplicationController
   def show
     style_id = params[:id]
     @style = $brewery_db.styles.find(style_id)
+    print "-----------"
+    print @style.og_max
+    print "-----------"
   end
 
 end
